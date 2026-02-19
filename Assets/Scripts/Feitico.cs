f87951fe119ed9ea752b2eb7118ea3462087f8b2
ruanhames <ruan_hames@estudante.sesisenai.org.br>
2026-02-19 15:52:18 -0300

    Feito script feitico
    Feito o script "feitiço" da bola de fogo.

---
+++ Assets/Scripts/Feitico.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Feitico : MonoBehaviour
+{
+
+    public float velocidade =1;
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+
+    {
+        rb=GetComponent<Rigidbody2D>();
+
+   rb.AddForce (Vector2.up * velocidade, ForceMode2D.Impulse);
+
+    Destroy(gameObject, 3f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
